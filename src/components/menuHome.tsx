c13d011221da89fefbb011e9b8e4ab791b471354
Faell4328 <rhafahenrique@hotmail.com>
2025-07-08 11:31:08 -0300

    creating a menu component

---
+++ src/components/menuHome.tsx
@@ -0,0 +1,203 @@
+import { AutoComplete, AutoCompleteProps, Button, Menu, MenuProps } from "antd";
+import { useEffect, useState } from "react";
+import { consultApiService } from "../service/consultApiService";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { SearchOutlined, UserOutlined } from '@ant-design/icons';
+
+type MenuItem = Required<MenuProps>["items"][number];
+
+interface ProductsProps{
+  id: string;
+  name: string;
+  price: string;
+  originalPrice: number,
+  promotionPrice: number,
+  imagesProduct: [{ imagemUrl: string }]
+}
+
+export default function MenuHome({ user }){
+  const nav = useRouter();
+
+  const [ menuItemsCategoryState, setMenuItemsCategoryState ] = useState<MenuItem[]>([]);
+  const [ menuItemsSeachState, setMenuItemsSeachState ] = useState<AutoCompleteProps['options']>([]);
+  const [ menuItemsUserState, setMenuItemsUserState ] = useState<MenuItem[]>([]);
+
+  useEffect(() => {
+    if(user == undefined && user == null){
+      return;
+    }
+  
+    async function getInformationCategory(){
+      const dataCategories = await consultApiService(nav, "GET", "/categorias", null);
+      
+      if(dataCategories?.data){
+        const category: { id: string, name: string }[] = dataCategories.data as unknown as { id: string, name: string }[];
+        const menuCategory = category.map((item) => {
+          return { label: (<Link href={`/categoria/${item.id}`}>{item.name}</Link>), key: item.id }
+        });
+
+        let menuItems: MenuItem[] = [];
+
+        if(user.role == "ADMIN"){
+          menuItems = [
+            {
+              label: (<Link href="/categorias">Categorias</Link>), key: "menu-categorys", children:[
+                { type: "group", label: "Admin" , children: [
+                  {
+                    label: "Adicionar categoria", key: "add-category"
+                  }
+                ]},
+                { type: "group", label: "Categorias" , children: menuCategory}
+              ]
+            }
+          ];
+        }
+        else{
+          menuItems = [
+            {
+              label: (<Link href="/categorias">Categorias</Link>), key: "menu-categorys", children:[
+                { type: "group", label: "categorias" , children: menuCategory}
+              ]
+            }
+          ];
+        }
+  
+        setMenuItemsCategoryState(menuItems);
+      }
+      else{
+        let menuItems: MenuItem[] = [];
+        if(user.role == "ADMIN"){
+          menuItems = [
+            {
+              label: (<Link href="/categorias">Categorias</Link>), key: "menu-categorys", children:[
+                { type: "group", label: "Admin" , children: [
+                  {
+                    label: (<Link href="/admin/categoria">Adicionar Categoria</Link>), key: "add-category"
+                  },
+                  {
+                    label: (<Link href="/admin/produto">Adicionar Produto</Link>), key: "add-product"
+                  },
+                ]}
+              ]
+            }
+          ];
+        }
+        setMenuItemsCategoryState(menuItems);
+      }
+    }
+  
+    async function menuSearchField(){
+      const returnApi = await consultApiService(nav, "GET", "/produtos", null);
+      
+      if(returnApi && returnApi?.data){
+        let products = returnApi.data as unknown as ProductsProps[];
+        if(products.length > 0){
+          let productsSearch: AutoCompleteProps['options'] = products.map((product) => {
+            return { key: product.id, label: product.name, value: product.name }
+          })
+          setMenuItemsSeachState(productsSearch);
+        }
+      }
+    }
+    
+    async function menuAccountField(){
+      if(user == "OFF"){
+        const menuItems: MenuItem[] = [
+          {
+            label: "", key: "menu-account" , icon: <UserOutlined />, children:[
+              { type: "group", label: "Conta" , children: [
+                { label: (<Link href="/login">Acessar</Link>), key:"1" },
+                { label: (<Link href="/cadastrar">Cadastrar</Link>), key:"2" }
+              ]}
+            ]
+          }
+        ];
+        setMenuItemsUserState(menuItems);
+      }
+      else if(user?.role == "USER"){
+        const menuItems: MenuItem[] = [
+          {
+            label: "", key: "menu-account", icon: <UserOutlined />, children:[
+              { type: "group", label: "Conta" , children: [
+                { label: (<div onClick={ () => { consultApiService(nav, "PUT", "/deslogar", null); nav.refresh() } }>Deslogar</div>), key:"1" },
+              ]}
+            ]
+          }
+        ];
+        setMenuItemsUserState(menuItems);
+      }
+      else if(user?.role == "ADMIN"){
+        const menuItems: MenuItem[] = [
+          {
+            label: "Conta", key: "menu-account", icon: <UserOutlined />, children:[
+              { type: "group", label: "Conta" , children: [
+                { label: (<div onClick={ () => { consultApiService(nav, "PUT", "/deslogar", null); nav.refresh() } }>Deslogar</div>), key:"1" },
+              ]}
+            ]
+          }
+        ];
+        setMenuItemsUserState(menuItems);
+      }
+    }
+  
+    async function index(){
+      getInformationCategory();
+      menuSearchField();
+      menuAccountField();
+    }
+  
+    index();
+  
+  }, []);
+
+  return(
+    <div>
+      { user == "OFF" &&
+        (
+          <div>
+            <div style={{ display: "flex", justifyContent: "space", width: "100%", borderBottom: "1px solid #eee" }}>
+              <Menu mode="horizontal" items={menuItemsCategoryState} style={{ width: "30%", display: "flex", justifyContent: "center", border: 0 }}/>
+              <div style={{ width: "50%", display: "flex", justifyContent: "center" }}>
+                <AutoComplete placeholder="Procurar produto" options={menuItemsSeachState} onSelect={() => {return "teste"}} filterOption={(inputValue, option) => { const label = option?.label;  if (typeof label === 'string') {return label.toLowerCase().includes(inputValue.toLowerCase());}return false;}}  style={{ width: "90%", height: "80%"}}/>
+                <Button type="primary" icon={<SearchOutlined/>} onClick={() => console.log("Clicou para pesquisar")} style={{ height: "80%" }} />
+              </div>
+              <Menu mode="horizontal" items={menuItemsUserState} style={{ width: "20%", display: "flex", justifyContent: "center", border: 0 }}/>
+            </div>
+            <p>Sua role é OFF</p>
+          </div>
+        )
+      }
+      { user?.role == "USER" &&
+        (
+          <div>
+            <div style={{ display: "flex", justifyContent: "space", width: "100%", borderBottom: "1px solid #eee" }}>
+              <Menu mode="horizontal" items={menuItemsCategoryState} style={{ width: "30%", display: "flex", justifyContent: "center", border: 0 }}/>
+              <div style={{ width: "50%", display: "flex", justifyContent: "center" }}>
+                <AutoComplete placeholder="Procurar produto" options={menuItemsSeachState} onSelect={() => {return "teste"}} filterOption={(inputValue, option) => { const label = option?.label;  if (typeof label === 'string') {return label.toLowerCase().includes(inputValue.toLowerCase());}return false;}}  style={{ width: "90%", height: "80%"}}/>
+                <Button type="primary" icon={<SearchOutlined/>} onClick={() => console.log("Clicou para pesquisar")} style={{ height: "80%" }} />
+              </div>
+              <Menu mode="horizontal" items={menuItemsUserState} style={{ width: "20%", display: "flex", justifyContent: "center", border: 0 }}/>
+            </div>
+            <p>Sua role é User</p>
+          </div>
+        )
+      }
+      { user?.role == "ADMIN" &&
+        (
+          <div>
+            <div style={{ display: "flex", justifyContent: "space", width: "100%", borderBottom: "1px solid #eee" }}>
+              <Menu mode="horizontal" items={menuItemsCategoryState} style={{ width: "30%", display: "flex", justifyContent: "center", border: 0 }}/>
+              <div style={{ width: "50%", display: "flex", justifyContent: "center" }}>
+                <AutoComplete placeholder="Procurar produto" options={menuItemsSeachState} onSelect={() => {return "teste"}} filterOption={(inputValue, option) => { const label = option?.label;  if (typeof label === 'string') {return label.toLowerCase().includes(inputValue.toLowerCase());}return false;}}  style={{ width: "90%", height: "80%"}}/>
+                <Button type="primary" icon={<SearchOutlined/>} onClick={() => console.log("Clicou para pesquisar")} style={{ height: "80%" }} />
+              </div>
+              <Menu mode="horizontal" items={menuItemsUserState} style={{ width: "20%", display: "flex", justifyContent: "center", border: 0 }}/>
+            </div>
+            <p>Sua role é Admin</p>
+          </div>
+        )
+      }
+    </div>
+  )
+}
